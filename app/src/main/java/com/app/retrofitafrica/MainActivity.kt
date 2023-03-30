86bc7a2d438665193ee96930f93948a957bac207
adrianreal <arealpalacios@safareyes.es>
2023-03-30 09:15:24 +0200

    Añadida paleta de colores y importado icono para botón de inicio y icono de aplicación

--- app/src/main/java/com/app/retrofitafrica/MainActivity.kt
+++ app/src/main/java/com/app/retrofitafrica/MainActivity.kt
@@ -1,52 +1,26 @@
 package com.app.retrofitafrica
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.app.AppCompatActivity
 import com.app.retrofitafrica.databinding.ActivityMainBinding
-import com.app.retrofitafrica.recyclerView.PaisAdapter
-import retrofit2.*
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val listaPaises = arrayListOf<PaisResponse>()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val call = getRetrofit().create(PaisAPI::class.java)
-
-        call.getPaises().enqueue(object : Callback<Map<String, PaisResponse>> {
-            override fun onResponse(call: Call<Map<String, PaisResponse>>, response: Response<Map<String, PaisResponse>>) {
-                if (response.isSuccessful) {
-                    val paises = response.body()
-                    paises?.forEach { (_, value) ->
-
-                        listaPaises.add(value)
-                    }
-
-                    initRecyclerView(listaPaises)
-                }
-            }
-            override fun onFailure(call: Call<Map<String, PaisResponse>>, t: Throwable) {
-
-            }
-        })
 
 
 
     }
 
-    private fun getRetrofit(): Retrofit {
+    /*private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://africapp-258b7-default-rtdb.europe-west1.firebasedatabase.app")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-    }
+    }*/
 
-    private fun initRecyclerView(listaPaises: ArrayList<PaisResponse>) {
-        binding.rvPaises.layoutManager = LinearLayoutManager(this)
-        binding.rvPaises.adapter = PaisAdapter(listaPaises)
-    }
+
 }
