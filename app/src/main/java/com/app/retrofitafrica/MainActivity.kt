b4d7c36d02c65392aed0bb23f5c87f22da1d82d7
Adrián Real Palacios <arealpalacios@safareyes.es>
2023-04-05 17:47:27 +0200

    Scrollview dinámico implementado

--- app/src/main/java/com/app/retrofitafrica/MainActivity.kt
+++ app/src/main/java/com/app/retrofitafrica/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.app.retrofitafrica
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.app.retrofitafrica.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    @RequiresApi(Build.VERSION_CODES.R)
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        val displayMetrics = resources.displayMetrics
+        val dpHeight = displayMetrics.heightPixels - 200
+        val layoutParams = binding.loginscreen.layoutParams
+        layoutParams.height = dpHeight
+        binding.loginscreen.layoutParams = layoutParams
 
 
     }
