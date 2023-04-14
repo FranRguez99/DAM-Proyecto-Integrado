e5e2ded02d6e7328c48077b09a1dbbd58fb23fff
Adrián Real Palacios <arealpalacios@safareyes.es>
2023-04-14 16:44:43 +0200

    Cambiado por completo la pantalla de LogIn con la funcionalidad de BottonSheet

--- app/src/main/java/com/app/retrofitafrica/MainActivity.kt
+++ app/src/main/java/com/app/retrofitafrica/MainActivity.kt
@@ -1,35 +1,30 @@
 package com.app.retrofitafrica
 
-import android.annotation.SuppressLint
-import android.os.Build
 import android.os.Bundle
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.app.retrofitafrica.databinding.ActivityMainBinding
+import com.app.retrofitafrica.databinding.LayoutLogInBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    @RequiresApi(Build.VERSION_CODES.R)
-    @SuppressLint("ClickableViewAccessibility")
+    private lateinit var logInBinding : LayoutLogInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val displayMetrics = resources.displayMetrics
-        val dpHeight = displayMetrics.heightPixels
+        binding.btOpenLogIn.setOnClickListener {
+            val bottomSheetDialog = BottomSheetDialog(this)
+            logInBinding = LayoutLogInBinding.inflate(layoutInflater)
+            bottomSheetDialog.setContentView(logInBinding.root)
+            // Toda funcionalidad del login deberá estar antes del show, para llamar a
+            // los elementos de la vista utilizar el logInBinding
 
+            bottomSheetDialog.show()
 
-        var layoutParams = binding.loginscreen.layoutParams
-        layoutParams.height = dpHeight - 200
-        binding.loginscreen.layoutParams = layoutParams
-
-        layoutParams = binding.imageView.layoutParams
-        layoutParams.height = dpHeight - 350
-        binding.imageView.layoutParams = layoutParams
-
-
+        }
 
     }
 
