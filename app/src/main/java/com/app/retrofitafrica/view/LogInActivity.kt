7ce8cf0754ce7b60826f41e328de6b5edcac46e4
Adrián Real Palacios <arealpalacios@safareyes.es>
2023-04-20 09:54:31 +0200

    Switch remember implementado

--- app/src/main/java/com/app/retrofitafrica/view/LogInActivity.kt
+++ app/src/main/java/com/app/retrofitafrica/view/LogInActivity.kt
@@ -3,27 +3,37 @@ package com.app.retrofitafrica.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.app.retrofitafrica.databinding.ActivityLogInBinding
-import com.app.retrofitafrica.viewmodel.LogInController
+import com.app.retrofitafrica.viewmodel.AuthController
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class LogInActivity : AppCompatActivity() {
     private lateinit var binding : ActivityLogInBinding
-    private lateinit var logInController: LogInController
+    private lateinit var authController: AuthController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLogInBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        logInController = LogInController(this)
-
+        authController = AuthController(this)
+        getSharedPreferences()
         binding.btLogIn.setOnClickListener {
             val email = binding.tietMail.text.toString()
             val pass = binding.tietPass.text.toString()
             CoroutineScope(Dispatchers.IO).launch {
-                logInController.login(email, pass)
+                authController.login(email, pass, binding.switch1.isChecked)
             }
         }
+    }
+
+    private fun getSharedPreferences() {
+        val preferences = getSharedPreferences("remember", MODE_PRIVATE)
+        binding.tietMail.setText(preferences.getString("email", "").toString())
+        binding.tietPass.setText(preferences.getString("pass", "").toString())
+        if (!binding.tietMail.text.isNullOrEmpty()) {
+            binding.switch1.isChecked = true
+        }
+
     }
 
 }
