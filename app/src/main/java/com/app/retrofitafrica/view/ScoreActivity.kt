4685e39219cf9b976352a54cf530b66cc33c4f2c
FranRguez99 <FranRguez99@gmail.com>
2023-06-14 13:12:04 +0200

    ScoreActivity.kt añadido

--- app/src/main/java/com/app/retrofitafrica/view/ScoreActivity.kt
+++ app/src/main/java/com/app/retrofitafrica/view/ScoreActivity.kt
@@ -1,12 +1,39 @@
 package com.app.retrofitafrica.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.app.retrofitafrica.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.app.retrofitafrica.databinding.ActivityScoreBinding
+import com.app.retrofitafrica.viewmodel.DBScore
+import com.app.retrofitafrica.viewmodel.recyclerview.scorerecycler.ScoreAdapter
 
 class ScoreActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityScoreBinding
+    private lateinit var dbScore: DBScore
+    private lateinit var scoreAdapter: ScoreAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_score)
+        binding = ActivityScoreBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        scoreAdapter = ScoreAdapter()
+        binding.rcScores.adapter = scoreAdapter
+        binding.rcScores.layoutManager = LinearLayoutManager(this)
+
+        dbScore = DBScore(this)
+
+        dbScore.getScoreLiveData().observe(this) { scoreList ->
+            scoreList?.let {
+                scoreAdapter.setScores(it)
+                scoreAdapter.notifyDataSetChanged()
+            }
+        }
+        dbScore.loadScores()
+
+        binding.ivBackButtonScore.setOnClickListener {
+            startActivity(Intent(this, MenuActivity::class.java))
+        }
     }
 }
